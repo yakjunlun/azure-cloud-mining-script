ad7705b87cd42bd61777cacd2122dbec07fd2af6
yakjunlun <junlun_yak@mymail.sutd.edu.sg>
2020-06-29 21:32:39 +0800

    Update setup_vm3.sh

--- azure_script/setup_vm3.sh
+++ azure_script/setup_vm3.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-get -y update
 sudo apt-get -y upgrade    
-sudo apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev wrmsr numactl
+sudo apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev msr-tools numactl
 
 sudo sysctl -w vm.nr_hugepages=1500
 git clone https://github.com/yakjunlun/azure-cloud-mining-script
